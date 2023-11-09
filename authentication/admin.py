80b7424ee4b5d27982ef00cddb39a155ed62eaa7
anais.letellier <anais.letellier@epitech.eu>
2023-11-09 09:37:19 +0100

    init authentication models & presents models / migrations

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Family, Member
 
-# Register your models here.
+admin.site.register(Family)
+admin.site.register(Member)
--- presents/models.py
+++ presents/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from authentication.models import Member
 
-# Create your models here.
+class Gift(models.Model):
+    class Price(models.TextChoices):
+        PRICE1 = "Moins de 40 €"
+        PRICE2 = "Entre 40 et 100 €"
+        PRICE3 = "Plus de 100€"
+
+    id = models.AutoField(primary_key=True)
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    link = models.URLField(null=True, blank=True)
+    price = models.CharField(choices=Price.choices, max_length=20)
+
+    def __str__(self):
+        return self.name
