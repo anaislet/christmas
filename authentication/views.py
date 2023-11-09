9ac440032d5a44dd36091b435b02a1ad43456176
anais.letellier <anais.letellier@epitech.eu>
2023-11-09 16:15:42 +0100

    change login form : add new member if unknown

--- authentication/views.py
+++ authentication/views.py
@@ -7,6 +7,7 @@ from django.views.generic import View
 def login_page(request):
     form = forms.CustomLoginForm()
     message = ''
+
     if request.method == 'POST':
         form = forms.CustomLoginForm(request.POST)
         if form.is_valid():
@@ -14,14 +15,27 @@ def login_page(request):
             member_name_form = form.cleaned_data.get('member_name')
             password_form = form.cleaned_data.get('family_password')
 
-            member_model = Member.objects.get(name= member_name_form)
-            family_model = Family.objects.get(name=member_model.family)
+            try:
+                member_model = Member.objects.get(name= member_name_form)
+                family_model = Family.objects.get(name=member_model.family)
 
-            if family_model.password != password_form:
-                message = "Ce mot de passe ne correspond à aucune famille enregistrée"
+                if family_model.password != password_form:
+                    message = "Ce mot de passe ne correspond à aucune famille enregistrée"
 
-            else:
-                request.session['member'] = member_name_form
-                return redirect("home")
+                else:
+                    request.session['member'] = member_name_form
+                    return redirect("home")
+
+            except Member.DoesNotExist:
+
+                try:
+                    new_member_family = Family.objects.get(password=password_form)
+                    new_member = Member.objects.create(name=member_name_form, family=new_member_family)
+                    print(new_member)
+                    request.session['member'] = new_member.name
+                    return redirect("home")
+                
+                except Family.DoesNotExist:
+                    message = "Ce mot de passe ne correspond à aucune famille enregistrée"
 
     return render(request, 'login.html', context={'form': form, 'message': message})
